5131059bee9cb3e4bcdfaa7b15077f8e4ca6209f
Apiruth <Aphiruth.s@baraadv.co.th>
2023-06-18 22:12:07 +0700

    panresponder acknownlege

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -5,13 +5,86 @@ import {Animated, View, StyleSheet, PanResponder, Text,TextInput} from 'react-na
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const [Boxs,appendBox] = useState([]);
+  const [propertyView,setPropertyView] = useState([]);
 
-  
+  const panResponder = React.useRef(
+    PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) =>
+        true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) =>
+        true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        setPropertyView(
+          ['onPanResponderGrant',
+            'moveX:\t'+gestureState.moveX,
+           'moveY:\t'+gestureState.moveY,
+           'stateID:\t'+gestureState.stateID,
+           'x0:\t'+gestureState.x0,
+           'y0:\t'+gestureState.y0,
+           'dx:\t'+gestureState.dx,
+           'dy:\t'+gestureState.dy,
+           'vx:\t'+gestureState.vx,
+           'vy:\t'+gestureState.vy
+          ]);
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        setPropertyView(
+          ['onPanResponderMove',
+            'moveX:\t'+gestureState.moveX,
+           'moveY:\t'+gestureState.moveY,
+           'stateID:\t'+gestureState.stateID,
+           'x0:\t'+gestureState.x0,
+           'y0:\t'+gestureState.y0,
+           'dx:\t'+gestureState.dx,
+           'dy:\t'+gestureState.dy,
+           'vx:\t'+gestureState.vx,
+           'vy:\t'+gestureState.vy
+          ]);
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) =>
+        true,
+      onPanResponderRelease: (evt, gestureState) => {
+        setPropertyView(
+          ['onPanResponderRelease',
+            'moveX:\t'+gestureState.moveX,
+           'moveY:\t'+gestureState.moveY,
+           'stateID:\t'+gestureState.stateID,
+           'x0:\t'+gestureState.x0,
+           'y0:\t'+gestureState.y0,
+           'dx:\t'+gestureState.dx,
+           'dy:\t'+gestureState.dy,
+           'vx:\t'+gestureState.vx,
+           'vy:\t'+gestureState.vy
+          ]);
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        setPropertyView(
+          ['onPanResponderTerminate',
+            'moveX:\t'+gestureState.moveX,
+           'moveY:\t'+gestureState.moveY,
+           'stateID:\t'+gestureState.stateID,
+           'x0:\t'+gestureState.x0,
+           'y0:\t'+gestureState.y0,
+           'dx:\t'+gestureState.dx,
+           'dy:\t'+gestureState.dy,
+           'vx:\t'+gestureState.vx,
+           'vy:\t'+gestureState.vy
+          ]);
+      },
+    }),
+  ).current;
+
   return (
     <View style={styles.container}>
-      <View style={styles.panResponderLayer}>Layer1</View>
-      <View style={styles.propLayer}>Layer2</View>
+      <View style={styles.panResponderLayer} {...panResponder.panHandlers}>Layer1</View>
+      <View style={styles.propLayer}>
+        <View style={styles.LayerHeader}><Text>Properties</Text></View>
+        <View style={styles.LayerDetail}><Text>{propertyView.map((Text)=><li key={Text}>{Text}</li>)}</Text></View>
+      </View>
 
     </View>
   );
@@ -32,19 +105,43 @@ const styles = StyleSheet.create({
   },
   panResponderLayer:{
     margin: '10px',
+    marginTop: '0px',
     padding: 15,
     flex: 0.7,
     height: '90%',
-    backgroundColor: '#c0c0c0',
-    //borderWidth: 1,
+    backgroundColor: '#d0f5ff',
+    borderWidth: 0.1,
+    borderColor: '#d0f5ff',
     borderRadius: 5,
     overflow: 'scroll',
   },
   propLayer:{
     flex: 0.3,
-    padding: 15,
+    borderRadius: 5,
+    flexDirection: 'column',
+    padding: 0,
+    borderWidth: 0.1,
+    borderColor: '#d0f5ff',
     backgroundColor: '#FFFFFF',
   },
+  LayerHeader:{
+    margin: 3,
+    backgroundColor: 'white',
+    fontSize: 18,
+    textAlign: 'left',
+    borderBottomWidth: 0.1,
+    borderColor: '#d0f5ff'
+  },
+  LayerDetail:{
+    margin: 3,
+    backgroundColor: '#d0f5ff',
+    fontSize: 16,
+    //fontFamily: './Bai_Jamjuree/BaiJamjuree-LightItalic.ttf',
+    textAlign: 'left',
+  },
+  Text:{
+    
+  }
 });
 
 export default App;
