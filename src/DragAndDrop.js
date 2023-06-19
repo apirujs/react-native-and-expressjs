136fd0d31d15780c920c370390b3bb95cb510782
Apiruth <Aphiruth.s@baraadv.co.th>
2023-06-19 17:04:39 +0700

    6/19/2023

--- src/DragAndDrop.js
+++ src/DragAndDrop.js
@@ -1,89 +1,94 @@
 //https://reactnative.dev/docs/panresponder?syntax=functional
 
-import React, {useRef , useState} from 'react';
-import {Animated, View, StyleSheet, PanResponder, Text,TextInput} from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { Animated, View, StyleSheet, PanResponder, Text, TextInput, Pressable } from 'react-native';
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const [propertyView,setPropertyView] = useState([]);
+  const [propertyView, setPropertyView] = useState([]);
+  const [Boxs, setBoxs] = useState([{ ID: '01', offsetX: 0, offsetY: 0, cX: 0, cY: 0 }]);
+  const [BoxFocusID, setBoxFocusID] = useState('');
+  const pan = useRef(new Animated.ValueXY()).current;
+
 
   const panResponder = React.useRef(
     PanResponder.create({
       // Ask to be the responder:
       onStartShouldSetPanResponder: (evt, gestureState) => true,
-      onStartShouldSetPanResponderCapture: (evt, gestureState) =>
-        true,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
-      onMoveShouldSetPanResponderCapture: (evt, gestureState) =>
-        true,
 
       onPanResponderGrant: (evt, gestureState) => {
-        setPropertyView(
+        /*setPropertyView(
           ['onPanResponderGrant',
-            'moveX:\t'+gestureState.moveX,
-           'moveY:\t'+gestureState.moveY,
-           'stateID:\t'+gestureState.stateID,
-           'x0:\t'+gestureState.x0,
-           'y0:\t'+gestureState.y0,
-           'dx:\t'+gestureState.dx,
-           'dy:\t'+gestureState.dy,
-           'vx:\t'+gestureState.vx,
-           'vy:\t'+gestureState.vy
-          ]);
+            'moveX:\t' + gestureState.moveX,
+            'moveY:\t' + gestureState.moveY,
+            'stateID:\t' + gestureState.stateID,
+            'x0:\t' + gestureState.x0,
+            'y0:\t' + gestureState.y0,
+            'dx:\t' + gestureState.dx,
+            'dy:\t' + gestureState.dy,
+            'vx:\t' + gestureState.vx,
+            'vy:\t' + gestureState.vy
+          ]);*/
+        setPropertyView([Boxs[0].offsetX, Boxs[0].offsetY]);
       },
       onPanResponderMove: (evt, gestureState) => {
-        setPropertyView(
-          ['onPanResponderMove',
-            'moveX:\t'+gestureState.moveX,
-           'moveY:\t'+gestureState.moveY,
-           'stateID:\t'+gestureState.stateID,
-           'x0:\t'+gestureState.x0,
-           'y0:\t'+gestureState.y0,
-           'dx:\t'+gestureState.dx,
-           'dy:\t'+gestureState.dy,
-           'vx:\t'+gestureState.vx,
-           'vy:\t'+gestureState.vy
-          ]);
+        pan.setValue({ x: gestureState.dx + Boxs[0].offsetX, y: gestureState.dy + Boxs[0].offsetY })
       },
       onPanResponderTerminationRequest: (evt, gestureState) =>
         true,
       onPanResponderRelease: (evt, gestureState) => {
-        setPropertyView(
+        /*setPropertyView(
           ['onPanResponderRelease',
-            'moveX:\t'+gestureState.moveX,
-           'moveY:\t'+gestureState.moveY,
-           'stateID:\t'+gestureState.stateID,
-           'x0:\t'+gestureState.x0,
-           'y0:\t'+gestureState.y0,
-           'dx:\t'+gestureState.dx,
-           'dy:\t'+gestureState.dy,
-           'vx:\t'+gestureState.vx,
-           'vy:\t'+gestureState.vy
-          ]);
+            'moveX:\t' + gestureState.moveX,
+            'moveY:\t' + gestureState.moveY,
+            'stateID:\t' + gestureState.stateID,
+            'x0:\t' + gestureState.x0,
+            'y0:\t' + gestureState.y0,
+            'dx:\t' + gestureState.dx,
+            'dy:\t' + gestureState.dy,
+            'vx:\t' + gestureState.vx,
+            'vy:\t' + gestureState.vy
+          ]);*/
+        Boxs[0].offsetX = Boxs[0].offsetX + gestureState.dx;
+        Boxs[0].offsetY = Boxs[0].offsetY + gestureState.dy;
+        setPropertyView([Boxs[0].offsetX, Boxs[0].offsetY]);
       },
       onPanResponderTerminate: (evt, gestureState) => {
-        setPropertyView(
+        /*setPropertyView(
           ['onPanResponderTerminate',
-            'moveX:\t'+gestureState.moveX,
-           'moveY:\t'+gestureState.moveY,
-           'stateID:\t'+gestureState.stateID,
-           'x0:\t'+gestureState.x0,
-           'y0:\t'+gestureState.y0,
-           'dx:\t'+gestureState.dx,
-           'dy:\t'+gestureState.dy,
-           'vx:\t'+gestureState.vx,
-           'vy:\t'+gestureState.vy
-          ]);
+            'moveX:\t' + gestureState.moveX,
+            'moveY:\t' + gestureState.moveY,
+            'stateID:\t' + gestureState.stateID,
+            'x0:\t' + gestureState.x0,
+            'y0:\t' + gestureState.y0,
+            'dx:\t' + gestureState.dx,
+            'dy:\t' + gestureState.dy,
+            'vx:\t' + gestureState.vx,
+            'vy:\t' + gestureState.vy
+          ]);*/
       },
     }),
   ).current;
 
   return (
     <View style={styles.container}>
-      <View style={styles.panResponderLayer} {...panResponder.panHandlers}>Layer1</View>
+      <View style={styles.panResponderLayer}>
+        <View style={styles.LayerHeader}><Text>View</Text></View>
+        <View style={styles.viewLayer} {...panResponder.panHandlers}>
+          <View>
+            <Animated.View style={[pan.getLayout()]}>
+              <Pressable id='01' onPress={(evt)=>{console.log('id: '+evt.currentTarget.id);}} style={styles.box}>
+                <View style={{ width: 'auto', height: 'auto'}} />
+              </Pressable>
+            </Animated.View>
+          </View>
+        </View>
+      </View>
+
       <View style={styles.propLayer}>
         <View style={styles.LayerHeader}><Text>Properties</Text></View>
-        <View style={styles.LayerDetail}><Text>{propertyView.map((Text)=><li key={Text}>{Text}</li>)}</Text></View>
+        <View style={styles.LayerDetail}><Text>{propertyView.map((Text) => <li key={Text}>{Text}</li>)} count:{count}</Text></View>
       </View>
 
     </View>
@@ -103,19 +108,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#9bff9b',
     overflow: 'hidden',
   },
-  panResponderLayer:{
+  panResponderLayer: {
     margin: '10px',
     marginTop: '0px',
-    padding: 15,
     flex: 0.7,
     height: '90%',
-    backgroundColor: '#d0f5ff',
-    borderWidth: 0.1,
+    backgroundColor: 'white',
     borderColor: '#d0f5ff',
     borderRadius: 5,
-    overflow: 'scroll',
+
   },
-  propLayer:{
+  viewLayer: {
+    padding: 15,
+    flexWrap: 'wrap',
+    overflow: 'scroll',
+    backgroundColor: '#d0f5ff',
+    height: '100%',
+  },
+  propLayer: {
     flex: 0.3,
     borderRadius: 5,
     flexDirection: 'column',
@@ -124,7 +134,7 @@ const styles = StyleSheet.create({
     borderColor: '#d0f5ff',
     backgroundColor: '#FFFFFF',
   },
-  LayerHeader:{
+  LayerHeader: {
     margin: 3,
     backgroundColor: 'white',
     fontSize: 18,
@@ -132,16 +142,22 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0.1,
     borderColor: '#d0f5ff'
   },
-  LayerDetail:{
+  LayerDetail: {
     margin: 3,
     backgroundColor: '#d0f5ff',
     fontSize: 16,
     //fontFamily: './Bai_Jamjuree/BaiJamjuree-LightItalic.ttf',
     textAlign: 'left',
   },
-  Text:{
-    
+  Text: {
+
   }
+  , box: {
+    backgroundColor: '#61dafb',
+    width: 80,
+    height: 80,
+    borderRadius: 4,
+  },
 });
 
 export default App;
